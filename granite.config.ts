9cd5e5cfb88ab8202840fd440d10b26e783b2338
mainandtop <momoseya55@gmail.com>
2026-04-23 12:55:46 +0900

    Add files via upload

--- granite.config.ts
+++ granite.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from '@apps-in-toss/web-framework/config';
 export default defineConfig({
   appName: 'retrodosa',
   brand: {
-    displayName: '슬롯머신-레트로 도사', // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
+    displayName: '슬롯머신 레트로도사', // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
     primaryColor: '#3182F6', // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
-    icon: '',
+    icon: 'https://static.toss.im/appsintoss/34157/6d99664a-ca41-40c5-a3dd-12130927e433.png',
   },
   web: {
     host: 'localhost',
